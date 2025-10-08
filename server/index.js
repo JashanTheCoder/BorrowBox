ddfd679c58310ef7e481adb5d48bdd9f226c3fef
Jashan <jashan72464@gmail.com>
2025-10-08 10:53:06 +0530

    maybe now

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(
 	cors({
-		origin: ['http://localhost:5173', 'https://borrow-box-five.vercel.app'],
+		origin: ['http://localhost:5173', 'https://borrow-box-five.vercel.app/api'],
 		credentials: true,
 	})
 );
